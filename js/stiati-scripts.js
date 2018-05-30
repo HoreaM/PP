000c3c69c128aa695f132e0b849d060fded3f3ef
HoreaM <horea_manita@yahoo.com>
2018-05-30 20:18:05 +0300

    Toggle Play on Audio End (Stiati)

--- js/stiati-scripts.js
+++ js/stiati-scripts.js
@@ -1,40 +1,44 @@
-var timer;
-var percent = 0;
-var audios = document.getElementsByClassName("audio");
-var playImgs = document.getElementsByClassName("playImage");
+let timer;
+let percent = 0;
+let audios = document.getElementsByClassName("audio");
+let playImgs = document.getElementsByClassName("playImage");
 
-var myIndex = 0;
+let myIndex = 0;
 carouselAhead();
 
-for (i=0; i < audios.length; i++) {
-    audios[i].oneneded = playHasEnded();
+for (i = 0; i < audios.length; i++) {
+    audios[i].addEventListener("ended", toggleOnEnd);
+}
+
+function toggleOnEnd() {
+    playImgs[myIndex - 1].src = "resources/images/play.png";
 }
 
 function carouselBehind() {
-    var i;
-    var x = document.getElementsByClassName("container");
+    let i;
+    let x = document.getElementsByClassName("container");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
 
     myIndex--;
 
-    if (myIndex == 0) {
+    if (myIndex === 0) {
         myIndex = x.length - 1;
     }
 
-    var behindButton = document.getElementById("behind");
-    var aheadButton = document.getElementById("ahead");
-    var goToMenuButton = document.getElementById("goToMenu");
-    var startAgainButton = document.getElementById("startAgain");
+    let behindButton = document.getElementById("behind");
+    let aheadButton = document.getElementById("ahead");
+    let goToMenuButton = document.getElementById("goToMenu");
+    let startAgainButton = document.getElementById("startAgain");
 
-    if (myIndex == 1) {
+    if (myIndex === 1) {
         behindButton.style.display = "none";
     } else {
         behindButton.style.display = "block";
     }
 
-    if (myIndex != x.length) {
+    if (myIndex !== x.length) {
         aheadButton.style.display = "block";
         goToMenuButton.style.display = "none";
         startAgainButton.style.display = "none";
@@ -44,8 +48,8 @@ function carouselBehind() {
 }
 
 function carouselAhead() {
-    var i;
-    var x = document.getElementsByClassName("container");
+    let i;
+    let x = document.getElementsByClassName("container");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
@@ -56,17 +60,17 @@ function carouselAhead() {
         myIndex = 1;
     }
 
-    var behindButton = document.getElementById("behind");
-    var aheadButton = document.getElementById("ahead");
-    var goToMenuButton = document.getElementById("goToMenu");
-    var startAgainButton = document.getElementById("startAgain");
+    let behindButton = document.getElementById("behind");
+    let aheadButton = document.getElementById("ahead");
+    let goToMenuButton = document.getElementById("goToMenu");
+    let startAgainButton = document.getElementById("startAgain");
 
-    if (myIndex == x.length) {
+    if (myIndex === x.length) {
         goToMenuButton.style.display = "block";
         startAgainButton.style.display = "block";
         aheadButton.style.display = "none";
         behindButton.style.display = "block";
-    } else if (myIndex == 1) {
+    } else if (myIndex === 1) {
         goToMenuButton.style.display = "none";
         startAgainButton.style.display = "none";
         aheadButton.style.display = "block";
@@ -81,63 +85,64 @@ function carouselAhead() {
     x[myIndex - 1].style.display = "block";
 }
 
-audio.addEventListener("playing", function(_event) {
-    var duration = _event.target.duration;
-    advance(duration, audio);
-});
+// audio.addEventListener("playing", function (_event) {
+//     let duration = _event.target.duration;
+//     advance(duration, audio);
+// });
+//
+// audio.addEventListener("pause", function (_event) {
+//     clearTimeout(timer);
+// });
 
-audio.addEventListener("pause", function(_event) {
-    clearTimeout(timer);
-});
-
-var startTimer = function(duration, element){
-    if(percent < 100) {
-        timer = setTimeout(function (){advance(duration, element)}, 100);
+let startTimer = function (duration, element) {
+    if (percent < 100) {
+        timer = setTimeout(function () {
+            advance(duration, element)
+        }, 100);
     }
 }
 
-function togglePlay (e) {
+function togglePlay(e) {
     e = e || window.event;
-    var btn = e.target;
-
-    if (!audios[myIndex-1].paused) {
+    let btn = e.target;
+    if (!audios[myIndex - 1].paused) {
         btn.classList.remove('active');
-        audios[myIndex-1].pause();
-        playImgs[myIndex-1].src = "resources/images/play.png";
+        audios[myIndex - 1].pause();
+        playImgs[myIndex - 1].src = "resources/images/play.png";
         isPlaying = false;
 
     } else {
         btn.classList.add('active');
-        audios[myIndex-1].play();
-        playImgs[myIndex-1].src = "resources/images/pause.png";
+        audios[myIndex - 1].play();
+        playImgs[myIndex - 1].src = "resources/images/pause.png";
         isPlaying = true;
     }
 
     function pausePlay() {
         e = e || window.event;
-        var btn = e.target;
+        let btn = e.target;
 
-        if (!audios[myIndex-1].paused) {
+        if (!audios[myIndex - 1].paused) {
             btn.classList.remove('active');
-            audios[myIndex-1].pause();
-            playImgs[myIndex-1].src = "resources/images/play.png";
+            audios[myIndex - 1].pause();
+            playImgs[myIndex - 1].src = "resources/images/play.png";
             isPlaying = false;
         }
     }
 
     function startPlay() {
         e = e || window.event;
-        var btn = e.target;
+        let btn = e.target;
 
-        if (audios[myIndex-1].paused) {
+        if (audios[myIndex - 1].paused) {
             btn.classList.add('active');
-            audios[myIndex-1].play();
-            playImgs[myIndex-1].src = "resources/images/pause.png";
+            audios[myIndex - 1].play();
+            playImgs[myIndex - 1].src = "resources/images/pause.png";
             isPlaying = true;
         }
     }
 
     function playHasEnded() {
-        playImgs[myIndex-1].src = "resources/images/play.png";
+        playImgs[myIndex - 1].src = "resources/images/play.png";
     }
 }
